d99e933ffb4ca0c49bc0013a87d80fbc90313c83
Poolynx <wesleyrab2020@gmail.com>
2024-01-29 21:16:10 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Position pos = new Position(9, 2);
+		System.out.println(pos);
 	}
 }
