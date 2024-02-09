0ea60707cada6e194e7e26cab5d727b778dec118
Poolynx <wesleyrab2020@gmail.com>
2024-02-09 08:41:46 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -85,7 +85,8 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		
 		if(capturedPiece != null) {
@@ -98,8 +99,9 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		 Piece p = board.removePiece(target);
-		 board.placePiece(p, source);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
+		board.placePiece(p, source);
 		 
 		 if(capturedPiece != null) {
 			 board.placePiece(capturedPiece, target);
